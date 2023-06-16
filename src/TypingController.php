36f98c2ba6f3d91a9b410f7f300302787668a391
gaurav <gaurav.thinktanker@gmail.com>
2023-06-16 14:02:52 +0530

    set design and menu

--- src/TypingController.php
+++ src/TypingController.php
@@ -1,16 +1,18 @@
 <?php
 namespace Laraveltyping\Typing;
 use Illuminate\Http\Request;
-use Input,DB,Auth;
+use DB;
 use App\Http\Controllers\Controller;
 
 class TypingController extends Controller
 {
-
-    public function index()
+   
+    public function index(Request $request)
     {   
-        $id = 111;
-        //$id = Auth::id();
+        
+
+        $user = session()->get('user');
+        $id = $user->id;
         $txtFile = file_get_contents(__DIR__.'\storage\paragraph.txt');
         $lines = preg_split('/[\n\r]+/', $txtFile);
         $sentence = $lines[array_rand($lines)];
@@ -30,7 +32,40 @@ class TypingController extends Controller
             {
                 $list_accuracy[$key]=$val['accuracy'];
             }
-        return view('typing::index',compact('sentence','list_speed','list_accuracy'));
+
+            $typing = [];
+            $status = [];
+            $status = ['status'=>"inactive",'pusher_app_key'=>'','pusher_cluster'=>'','force_tls'=>''];
+            $status = (object) ($status);
+            $appTheme = ['sidebar_theme'=>'dark','header_color'=>''];
+            $appTheme =(object) ($appTheme);
+            $sidebarUserPermissions = [];
+            $sidebarUserPermissions = ['view_overview_dashboard'=>4,'add_employees'=>4,'view_lead'=>5,'view_clients'=>5,'view_employees'=>5,'view_leave'=>5,'view_attendance'=>5,'view_holiday'=>5,'view_contract'=>5,'view_projects'=>5,'view_tasks'=>5,'view_timelogs'=>5,'view_estimates'=>5,'view_invoices'=>5,'view_payments'=>5,'view_expenses'=>5,'view_lead_proposals'=>5,'view_bankaccount'=>5,'view_tickets'=>5,'view_events'=>5,'view_notice'=>5,'view_task_report'=>5,'view_time_log_report'=>5,'view_finance_report'=>5,'view_income_expense_report'=>5,'view_leave_report'=>5,'view_attendance_report'=>5,'manage_company_setting'=>4];
+            //$status = json_decode($status);
+         //   $status = json_encode($status,true);
+           // dd($status);
+           $worksuitePlugins = [];
+           $customLink = [];
+            $this->checkListCompleted = 5;
+            $this->checkListTotal = 6;
+            $this->pushSetting = $status;
+            $this->user =$user;
+            $this->appTheme = $appTheme;
+            $this->worksuitePlugins = $worksuitePlugins;
+            $this->pusherSettings = $status;
+             $this->sentence = $sentence;
+             $this->activeTimerCount=0;
+             $this->customLink = $customLink;
+             $this->unreadMessagesCount = 0;
+             $this->sidebarUserPermissions=$sidebarUserPermissions;
+             $this->unreadNotificationCount =0;
+            $this->pageTitle= "Typing";
+             $this->list_speed = $list_speed;
+             $this->list_accuracy = $list_accuracy;
+             $this->typing = $typing;
+            
+            
+        return view('typing::index',$this->data);
     }
 
     public function test_ajax(Request $request)
@@ -43,10 +78,8 @@ class TypingController extends Controller
 
     public function test_data(Request $request)
     {
-
-        //$id = Auth::id();
-        $id = 111;
-       
+        $user = session()->get('user');
+        $id = $user->id;
         DB::table('typingspeed')->where('user_id',$id)->insert([
             'speed' => $request->NWPM,
             'user_id' => $id,
--- src/routes/web.php
+++ src/routes/web.php
@@ -1,13 +1,10 @@
 <?php
 use Illuminate\Support\Facades\Route;
-
-Route::get('timezones/{timezone}',
-'laraveltlr\tlr\TlrController@index');
-
+Route::group(['middleware' => 'web'], function () {
 Route::get('typing',
 'laraveltyping\typing\TypingController@index');
 Route::post('test2',
 'laraveltyping\typing\TypingController@test_ajax');
-
 Route::post('/test_data',
 'laraveltyping\typing\TypingController@test_data');
+});
